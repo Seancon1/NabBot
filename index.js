3853615f9b9db8da09099fa23ecf8553afa52c41
Sean <sean@prestigecode.com>
2021-01-25 18:07:22 -0500

    More tutorial additions

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
+const fs = require('fs');
 const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+const { Console } = require('console');
+
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.login(process.env.TOKEN);
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	try{
+		switch(command) {
+			case 'nab':
+				message.channel.send('Oh shit waddup: ' + args);
+				break;
+		}
+	} catch (error) {
+		console.error(error);
+	}
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
+
+client.login(token);
